f00b675fa9bacfc8a17d6e690ae0dee09e3204e4
LuisSantamariaUPB <luisestebansantamaria@outlook.com>
2020-03-07 17:32:22 -0500

    Antes de Optimizar funciones 1.0

--- QuizMusic/js/main.js
+++ QuizMusic/js/main.js
@@ -1,4 +1,5 @@
 var indexArray = 0;
+var secciones = [];
 indexIntBar = 0;
 var resp = [
     ["", "", "", ""],
@@ -18,6 +19,18 @@ window.onload = function () {
 
 }
 
+function inicializarReferencias(){
+    secciones[1] = document.getElementById("seccion_1");
+    secciones[2] = document.getElementById("seccion_2");
+    secciones[3] = document.getElementById("seccion_3");
+    secciones[4] = document.getElementById("seccion_4");
+    secciones[5] = document.getElementById("seccion_5");
+}
+
+function cambiarSplash(){
+    secciones[1].className = "splash oculto";
+    secciones[2].className = "home";
+}
 
 function toHome() {
     document.getElementById("QuizMusic").className = "animated flipInX";
